f42b2aff9eb027e3d521924dce589065d2a13fce
jheebl <jheebl@umich.edu>
2015-09-23 00:58:49 -0400

    powerUps move w/map, and destroy offScreen()

---
+++ Assets/PowerUpMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpMovement : MonoBehaviour
+{
+    public GameObject pUp;
+
+    // camera info
+    float camH, camW;
+    float speed = 3f;    // speed of the map moving  
+                         // Use this for initialization
+    void Start ()
+    {
+        //Camera Initialization
+        Camera cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        camH = cam.orthographicSize * 2f;
+        camW = camH * cam.aspect;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pUp.transform.position += Time.deltaTime * new Vector3(-speed, 0f, 0f);
+        OffCamera();
+    }
+
+    public void OffCamera()
+    {
+        if (this.transform.position.x >= (camW / 2 + 2) || this.transform.position.x <= (-camW / 2 - 2))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
