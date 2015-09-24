b50dc3f7c43f7eb08146b19b382a5824b1dffd1c
comstar <comstar@umich.edu>
2015-09-24 15:15:47 -0400

    PowerUp Hopper added

--- Assets/_Scripts/Scheduler.cs
+++ Assets/_Scripts/Scheduler.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.IO;
 
-enum enemyID{fan, div, divS, osc, oscP, gnd, tur, turP, silo, hop, ATST};
+enum enemyID{fan, div, divS, osc, oscP, gnd, tur, turP, silo, hop, hopP, ATST};
 
 public class Scheduler : MonoBehaviour {
 
