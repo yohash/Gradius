3de738e01f87741278f8869cc317114abd209286
comstar <comstar@umich.edu>
2015-09-26 20:57:23 -0400

    Started FlrCeil sprites

--- Assets/_Scripts/FloorScroller.cs
+++ Assets/_Scripts/FloorScroller.cs
@@ -7,22 +7,27 @@ public class FloorScroller : MonoBehaviour
 
     float speed = 3f;    // speed of the map moving  
 
+	float camW, camH;
+
     // Use this for initialization
     void Start()
     {
+		Camera cam = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		camH = cam.orthographicSize * 2f;
+		camW = camH * cam.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
         // initially, the platform floats out and pauses once it's on screen
-        if (map.transform.position.x > 0)
+        if (map.transform.position.x > -camW/2)
         {
             map.transform.position += Time.deltaTime * new Vector3(-speed, 0f, 0f);
         }
         else
         {
-            map.transform.position = Vector3.zero;
+            //map.transform.position = Vector3.zero;
         }
     }
 }
